68761b7cc3b8e34caeed37a37426c82a9e16c17c
Thein Htut <theinhtut.dec@gmail.com>
2019-12-07 15:53:47 +0800

    Integrated edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,6 +18,22 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is updated.'}
+      else
+        format.html { render :edit }
+      end
+
+    end
+  end
+
   private
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
